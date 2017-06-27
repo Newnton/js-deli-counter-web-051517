0dd55de3a5720dcb7941c77a6011b7e00a740369
Zach Dubno <zachedubno@gmail.com>
2017-06-27 09:44:01 -0400

    finished lab

---
+++ index.js
@@ -0,0 +1,33 @@
+let katzDeliLine = [];
+
+function takeANumber(katzDeliLine, name){
+  katzDeliLine.push(name)
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+}
+
+function nowServing(katzDeliLine){
+  if(katzDeliLine.length > 0){
+    return `Currently serving ${katzDeliLine.shift()}.`
+  }
+  else {
+    return "There is nobody waiting to be served!"
+  }
+}
+
+function currentLine(katzDeliLine){
+  if(katzDeliLine.length > 0){
+    let outString = `The line is currently:`
+    for(let i = 0; i < katzDeliLine.length; i++){
+      if(i === 0){
+        outString = outString + ` ${i + 1}. ${katzDeliLine[i]}`
+      }
+      else{
+        outString = outString + `, ${i + 1}. ${katzDeliLine[i]}`
+      }
+    }
+    return outString
+  }
+  else {
+    return "The line is currently empty."
+  }
+}
